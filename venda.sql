6ddd4e4fcdaa08ef60283a2909d7db802bcb174f
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2025-09-01 10:58:07 -0300

    banco marcel

--- venda.sql
+++ venda.sql
@@ -58,3 +58,14 @@ id int primary key auto_increment,
 consumivel_id int (11)
 );
 
+create table pet( 
+nome varchar(255),
+idade int(11),
+raca varchar(255),
+porte varchar(255),
+sexo varchar(255),
+responsavel varchar(255),
+id int(11),
+perfil_id int(11)
+);
+
