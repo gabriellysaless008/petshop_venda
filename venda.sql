da3dbfd06a68ab5e51867ee5f8e8eb56403e242a
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2025-09-01 10:49:29 -0300

    banco lavinia

--- venda.sql
+++ venda.sql
@@ -28,3 +28,19 @@ create table agenda(
     id int(11)
 );
 
+create table loja(
+	nome varchar(255),
+	email varchar(255),
+	telefone int(11),
+	cnpj varchar(255),
+	endereco varchar(255),
+	id int(11),
+	pet_id int(11),
+	catalogo_id int(11)
+);
+
+create table perfil(
+	fotoUrl varchar(255),
+	id int(11)
+);
+
