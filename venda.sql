e231be31b07ab2c1f0b862f064e495cdc470fdef
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2025-09-01 10:45:19 -0300

    banco fernanda

--- venda.sql
+++ venda.sql
@@ -12,3 +12,19 @@ create table venda(
     id int(11)
 );
 
+create table login(
+	user varchar(255),
+	passaword varchar(255),
+	id int(11),
+	login_id int(11),
+	responsavel_id int(11)
+);
+
+create table agenda(
+	data date,
+	horainicio varchar(255),
+    horafim varchar(255),
+    disponivel varchar(255),
+    id int(11)
+);
+
